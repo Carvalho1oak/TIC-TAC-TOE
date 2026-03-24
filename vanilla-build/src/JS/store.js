5d984b097b5b505a62bad3d5a468ef75dcede85c
Gabriel Diogenes P. De Carvalho <gabrieldiogenes96@gmail.com>
2026-03-24 13:28:34 -0300

    More Refactoring
    Game is Also Playable Somewhat

--- vanilla-build/src/JS/store.js
+++ vanilla-build/src/JS/store.js
@@ -1,14 +1,68 @@
 const initialValue = {
-    moves: [],
-}
+  moves: [],
+};
 
 export default class Store {
   #state = initialValue;
 
-  constructor() {}
+  constructor(players) {
+    this.players = players;
+  }
 
   get game() {
-    return "dummy"
+    const state = this.#getState();
+
+    const currentPlayer = this.players[state.moves.length % 2];
+
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    let winner = null;
+
+    for (const player of this.players) {
+      const selectedSquareIds = state.moves
+        .filter((move) => move.player.id === player.id)
+        .map((move) => move.squareId);
+
+      for (const pattern of winningPatterns) {
+        if (pattern.every((v) => selectedSquareIds.includes(v))) {
+          winner = player;
+        }
+      }
+    }
+
+    return {
+      moves: state.moves,
+      currentPlayer,
+      status: {
+        isComplete: winner != null || state.moves.length === 9,
+        winner,
+      },
+    };
+  }
+
+  playerMove(squareId) {
+    const state = this.#getState();
+
+    const stateClone = structuredClone(state);
+
+    stateClone.moves.push({
+      squareId,
+      player: this.game.currentPlayer,
+    });
+
+    this.#saveState(stateClone);
+  }
+
+  reset() {
+    this.#saveState(initialValue)
   }
 
   #getState() {
@@ -28,7 +82,7 @@ export default class Store {
         newState = stateOrFn;
         break;
       default:
-        throw new Error("Invalid argument passed to savaState");
+        throw new Error("Invalid argument passed to saveState");
     }
 
     this.#state = newState;
--- vanilla-build/src/JS/view.js
+++ vanilla-build/src/JS/view.js
@@ -26,6 +26,7 @@ export default class View {
 
   bindGameResetEvent(handler) {
     this.$.resetBtn.addEventListener("click", handler);
+    this.$.modalBtn.addEventListener("click", handler);
   }
 
   bindNewRoundEvent(handler) {
@@ -34,13 +35,26 @@ export default class View {
 
   bindPlayerMoveEvent(handler) {
     this.$$.squares.forEach((square) => {
-      square.addEventListener("click", handler);
+      square.addEventListener("click", () => handler(square));
     });
   }
 
   /**
    * DOM helper methods
    */
+  openModal(message) {
+    this.$.modal.classList.remove("hidden");
+    this.$.modalText.innerText = message;
+  }
+  closeModal() {
+    this.$.modal.classList.add("hidden");
+  }
+  clearMoves() {
+    this.$$.squares.forEach((square) => {
+      square.replaceChildren();
+    });
+  }
+
   #toggleMenu() {
     this.$.menuItems.classList.toggle("hidden");
     this.$.menuBtn.classList.toggle("border");
@@ -53,25 +67,18 @@ export default class View {
 
   handlePlayerMove(squareEl, player) {
     const icon = document.createElement("i");
-    icon.classList.add(
-      "fa-solid",
-      player === 1 ? "fa-x" : "fa-o",
-      player === 1 ? "yellow" : "turquoise",
-    );
+    icon.classList.add("fa-solid", player.iconClass, player.colorClass);
     squareEl.replaceChildren(icon);
   }
 
-  // player = 1 | 2
   setTurnIndicator(player) {
     const icon = document.createElement("i");
     const label = document.createElement("p");
 
-    this.$.turn.classList.add(player === 1 ? "yellow" : "turquoise");
-    this.$.turn.classList.remove(player === 1 ? "turquoise" : "yellow");
+    icon.classList.add("fa-solid", player.colorClass, player.iconClass);
 
-    icon.classList.add("fa-solid", player === 1 ? "fa-x" : "fa-o");
-    label.innerText =
-      player === 1 ? "Player 1, you're up!" : "Player 2, you're up!";
+    label.classList.add(player.colorClass);
+    label.innerText = `${player.name}, you're up!`;
 
     this.$.turn.replaceChildren(icon, label);
   }
