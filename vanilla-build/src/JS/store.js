bbacb2aa59d5a5d997b8be1b770061370360129c
Gabriel Diogenes P. De Carvalho <gabrieldiogenes96@gmail.com>
2026-03-26 18:12:11 -0300

    Game is done
    this project overall was pretty fun, typescript refactor next

--- vanilla-build/src/JS/store.js
+++ vanilla-build/src/JS/store.js
@@ -1,18 +1,42 @@
 const initialValue = {
-  moves: [],
+  currentGameMoves: [],
+  history: {
+    currentRoundGames: [],
+    allGames: [],
+  },
 };
 
-export default class Store {
-  #state = initialValue;
-
-  constructor(players) {
+export default class Store extends EventTarget {
+  constructor(key, players) {
+    super();
+    this.storagekey = key;
     this.players = players;
+  }
+
+  get stats() {
+    const state = this.#getState();
+
+    return {
+      playerWithStats: this.players.map((player) => {
+        const wins = state.history.currentRoundGames.filter(
+          (game) => game.status.winner?.id === player.id,
+        ).length;
+
+        return {
+          ...player,
+          wins,
+        };
+      }),
+      ties: state.history.currentRoundGames.filter(
+        (game) => game.status.winner === null,
+      ).length,
+    };
   }
 
   get game() {
     const state = this.#getState();
 
-    const currentPlayer = this.players[state.moves.length % 2];
+    const currentPlayer = this.players[state.currentGameMoves.length % 2];
 
     const winningPatterns = [
       [1, 2, 3],
@@ -27,7 +51,7 @@ export default class Store {
     let winner = null;
 
     for (const player of this.players) {
-      const selectedSquareIds = state.moves
+      const selectedSquareIds = state.currentGameMoves
         .filter((move) => move.player.id === player.id)
         .map((move) => move.squareId);
 
@@ -39,21 +63,19 @@ export default class Store {
     }
 
     return {
-      moves: state.moves,
+      moves: state.currentGameMoves,
       currentPlayer,
       status: {
-        isComplete: winner != null || state.moves.length === 9,
+        isComplete: winner != null || state.currentGameMoves.length === 9,
         winner,
       },
     };
   }
 
   playerMove(squareId) {
-    const state = this.#getState();
+    const stateClone = structuredClone(this.#getState());
 
-    const stateClone = structuredClone(state);
-
-    stateClone.moves.push({
+    stateClone.currentGameMoves.push({
       squareId,
       player: this.game.currentPlayer,
     });
@@ -62,11 +84,36 @@ export default class Store {
   }
 
   reset() {
-    this.#saveState(initialValue)
+    const stateClone = structuredClone(this.#getState());
+
+    const { status, moves } = this.game;
+
+    if (status.isComplete) {
+      const { moves } = this.game;
+      stateClone.history.currentRoundGames.push({
+        moves,
+        status,
+      });
+    }
+
+    stateClone.currentGameMoves = [];
+
+    this.#saveState(stateClone);
+  }
+
+  newRound() {
+    this.reset();
+
+    const stateClone = structuredClone(this.#getState());
+    stateClone.history.allGames.push(...stateClone.history.currentRoundGames);
+    stateClone.history.currentRoundGames = [];
+
+    this.#saveState(stateClone);
   }
 
   #getState() {
-    return this.#state;
+    const item = window.localStorage.getItem(this.storagekey);
+    return item ? JSON.parse(item) : initialValue;
   }
 
   #saveState(stateOrFn) {
@@ -85,6 +132,7 @@ export default class Store {
         throw new Error("Invalid argument passed to saveState");
     }
 
-    this.#state = newState;
+    window.localStorage.setItem(this.storagekey, JSON.stringify(newState));
+    this.dispatchEvent(new Event("statechange"));
   }
 }
--- vanilla-build/src/JS/view.js
+++ vanilla-build/src/JS/view.js
@@ -12,12 +12,40 @@ export default class View {
     this.$.modalText = this.#qs('[data-id="modal-text"]');
     this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
     this.$.turn = this.#qs('[data-id="turn"]');
-
     this.$$.squares = this.#qsAll('[data-id="square"]');
+    this.$.p1Wins = this.#qs('[data-id="p1-wins"]');
+    this.$.p2Wins = this.#qs('[data-id="p2-wins"]');
+    this.$.ties = this.#qs('[data-id="ties"]');
+    this.$.grid = this.#qs('[data-id="grid"]');
     // UI-only event listeners
     this.$.menuBtn.addEventListener("click", (event) => {
       this.#toggleMenu();
     });
+  }
+
+  render(game, stats) {
+    const { playerWithStats, ties } = stats;
+    const {
+      moves,
+      currentPlayer,
+      status: { isComplete, winner },
+    } = game;
+
+    this.#closeAll();
+    this.#clearMoves();
+    this.#updateScoreboard(
+      playerWithStats[0].wins,
+      playerWithStats[1].wins,
+      ties,
+    );
+    this.#initializeMoves(moves);
+
+    if (isComplete) {
+      this.#openModal(winner ? `${winner.name} wins!` : "Tie!");
+      return;
+    }
+
+    this.#setTurnIndicator(currentPlayer);
   }
 
   /**
@@ -34,25 +62,52 @@ export default class View {
   }
 
   bindPlayerMoveEvent(handler) {
-    this.$$.squares.forEach((square) => {
-      square.addEventListener("click", () => handler(square));
-    });
+    this.#delegate(this.$.grid, '[data-id="square"]', "click", handler);
   }
 
   /**
    * DOM helper methods
    */
-  openModal(message) {
+  #updateScoreboard(p1Wins, p2Wins, ties) {
+    this.$.p1Wins.innerText = `${p1Wins} wins`;
+    this.$.p2Wins.innerText = `${p2Wins} wins`;
+    this.$.ties.innerText = `${ties} ties`;
+  }
+
+  #openModal(message) {
     this.$.modal.classList.remove("hidden");
     this.$.modalText.innerText = message;
   }
-  closeModal() {
+  #closeModal() {
     this.$.modal.classList.add("hidden");
   }
-  clearMoves() {
+  #closeAll() {
+    this.#closeModal();
+    this.#closeMenu();
+  }
+  #clearMoves() {
     this.$$.squares.forEach((square) => {
       square.replaceChildren();
     });
+  }
+
+  #initializeMoves(moves) {
+    this.$$.squares.forEach((square) => {
+      const existingMove = moves.find((move) => move.squareId === +square.id);
+
+      if (existingMove) {
+        this.#handlePlayerMove(square, existingMove.player);
+      }
+    });
+  }
+
+  #closeMenu() {
+    this.$.menuItems.classList.add("hidden");
+    this.$.menuBtn.classList.remove("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+    icon.classList.add("fa-chevron-up");
+    icon.classList.remove("fa-chevron-down");
   }
 
   #toggleMenu() {
@@ -65,13 +120,13 @@ export default class View {
     icon.classList.toggle("fa-chevron-up");
   }
 
-  handlePlayerMove(squareEl, player) {
+  #handlePlayerMove(squareEl, player) {
     const icon = document.createElement("i");
     icon.classList.add("fa-solid", player.iconClass, player.colorClass);
     squareEl.replaceChildren(icon);
   }
 
-  setTurnIndicator(player) {
+  #setTurnIndicator(player) {
     const icon = document.createElement("i");
     const label = document.createElement("p");
 
@@ -99,5 +154,12 @@ export default class View {
     if (!elList) throw new Error("Could not find elements");
 
     return elList;
+  }
+  #delegate(el, selector, eventKey, handler) {
+    el.addEventListener(eventKey, (event) => {
+      if (event.target.matches(selector)) {
+        handler(event.target);
+      }
+    });
   }
 }
